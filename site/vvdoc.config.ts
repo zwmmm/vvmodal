36b4ffb008ee152d240ed38b166dbfde816676e9
wayne <wei.zheng02@weimob.com>
2022-06-04 14:50:04 +0800

    更新文档

---
+++ site/vvdoc.config.ts
@@ -0,0 +1,71 @@
+import { Config, BuildConfig } from 'vvdoc'
+
+export const config: Config = {
+  title: 'vvModal',
+  logo: '/vvmodal.png',
+  repository: 'https://github.com/zwmmm/vvModal',
+  docsearch: {
+    appId: '',
+    indexName: '',
+    apiKey: ''
+  },
+  menus: [
+    {
+      text: '首页',
+      active: '^/',
+      path: '/'
+    },
+    {
+      text: 'API',
+      active: '^/apis',
+      path: '/apis/'
+    }
+  ],
+  chapters: {
+    '/apis/': [
+      {
+        name: 'Apis',
+        children: [
+          {
+            name: 'create',
+            path: '/apis/'
+          },
+          {
+            name: 'show',
+            path: '/apis/show'
+          },
+          {
+            name: 'antdModal',
+            path: '/apis/antdModal'
+          },
+          {
+            name: 'antdDrawer',
+            path: '/apis/antdDrawer'
+          }
+        ]
+      },
+      {
+        name: 'Hooks',
+        children: [
+          {
+            name: 'useModal',
+            path: '/apis/useModal'
+          },
+          {
+            name: 'useShow',
+            path: '/apis/useShow'
+          },
+          {
+            name: 'useHide',
+            path: '/apis/useHide'
+          }
+        ]
+      }
+    ]
+  },
+  theme: {
+    styles: {
+      colors: {}
+    }
+  }
+}
